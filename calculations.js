ead60a92fd85a780f8fcb846d1dce6496299d7bd
Zachary Caldwell <mintyst.john@gmail.com>
2021-09-30 13:44:32 -0700

    add test running

---
+++ calculations.js
@@ -0,0 +1,7 @@
+export function add_two(num){
+    return num + 2;
+}
+
+export function add(num1, num2) {
+    return num1 + num2;
+}
--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,31 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
 
+import { add_two, add } from '../calculations.js';
+
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('test add_two fucntion', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = 4;
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = add_two(2);
 
     //Expect
     // Make assertions about what is expected versus the actual result
+    expect.equal(actual, expected);
+});
+
+test('add function', (expect) => {
+    const input1 = 3;
+    const input2 = 2;
+    const expected = 5;
+
+    //act
+    const actual = add(input1, input2);
+
     expect.equal(actual, expected);
 });
